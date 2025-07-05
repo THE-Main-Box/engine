4e9727c4b69df28f1fd8ec1a9c08f55ed961282b
Thiago Barbosa <curiosidades381@gmail.com>
2025-07-05 05:10:20 -0300

    REFACT-DEBUG
    *Bug  de ajuste de pool corrijido
    (Antes eliminávamos toda a pool e assim removiamos junto projéteis que não foram destruidos, causando uma pequena inconsistencia, mas agora readicionamos eles denovo, logo após removermos tudo, como só será chamado de tempos em tempos não será um problema muito grande)
    
    *sistema de eliminação de objetos excessivos
    (Agora as pools podem eliminar, destruir, seus projéteis inativos periódicamente,e todas as pools que não tem projéteis são eliminadas)

---
+++ core/src/main/java/official/sketchBook/util_related/info/values/constants/ProjectileManagementConstants.java
@@ -0,0 +1,10 @@
+package official.sketchBook.util_related.info.values.constants;
+
+public class ProjectileManagementConstants {
+
+    public static int maxToDestroyPerFrame = 16;
+    public static int cleanInterval = 8;
+
+    public static int deleteInterval = 16;
+
+}

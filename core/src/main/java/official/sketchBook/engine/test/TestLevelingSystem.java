2144225d57ced18a0e1c0a93ea670ca79bd8d445
Thiago Barbosa <curiosidades381@gmail.com>
2025-10-27 14:47:51 -0300

    FEATURE-ADD-TEST-REFACT
    *Adição de componente de nível
    (Agora podemos adicionar e remover níveis de acordo com a nossa vontade)
    
    *Adição de testes
    (Adicionamos um pacote para testar componentes de lógica interna, e movemos alguns desses testes para dentro de tal pacote)

---
+++ core/src/main/java/official/sketchBook/engine/test/TestLevelingSystem.java
@@ -0,0 +1,50 @@
+package official.sketchBook.engine.test;
+
+import official.sketchBook.engine.components_related.integration_interfaces.LevelUpHolderII;
+import official.sketchBook.engine.components_related.toUse_component.object.LevelingComponent;
+
+public class TestLevelingSystem {
+
+    public static void main(String[] args) {
+
+
+        LevelUpHolderII holder = new LevelUpHolderII() {
+            @Override
+            public void onLevelUp(LevelingComponent lvlC) {
+                System.out.println("yay");
+            }
+
+            @Override
+            public void onLevelDown(LevelingComponent lvlC) {
+
+                System.out.println("aaa");
+            }
+
+            @Override
+            public LevelingComponent getLvlC() {
+                return null;
+            }
+        };
+
+        LevelingComponent lvl = new LevelingComponent(holder);
+
+        lvl.init(
+            0,
+            5,
+            0,
+            2
+        );
+
+        lvl.addProgress(14);
+        printLVL(lvl);
+
+        lvl.removeProgress(12);
+        printLVL(lvl);
+    }
+
+    private static void printLVL(LevelingComponent lvl){
+        System.out.println("Current LVL: " + lvl.getCurrentLvl() + "/" + lvl.getMaxLvl());
+        System.out.println("Current pro: " + lvl.getCurrentLvlProgress() + "/" + lvl.getMaxLvlProgress());
+        System.out.println("//--///---//---");
+    }
+}
